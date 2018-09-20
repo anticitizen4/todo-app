2c305690a55ec6c9d5b0c425ec98c5fc9d17dac6
anticitizen4 <20382674+anticitizen4@users.noreply.github.com>
2018-09-20 13:26:53 +0300

    make list persist between reloads

--- main.js
+++ main.js
@@ -6,6 +6,33 @@ let list = document.querySelector(".main__list");
 
 let footer_text = document.querySelector(".footer__text");
 
+let storage = {
+	add(string) {
+		let data = this.entries;
+		data.push(string);
+		this.entries = data;
+	},
+
+	remove(index) {
+		let data = this.entries;
+		data.splice(index, 1);
+		this.entries = data;
+	},
+};
+Object.defineProperty(storage, 'entries', {
+	get() {
+		let data = [];
+		if (localStorage.entries) {
+			data = JSON.parse(localStorage.entries);
+		}
+		return data;
+	},
+
+	set(data) {
+		localStorage.entries = JSON.stringify(data);
+	},
+});
+
 input_button.addEventListener("click", _ => {
 	let li = document.createElement("li");
 	li.textContent = input_field.value;
@@ -14,6 +41,8 @@ input_button.addEventListener("click", _ => {
 	let close_button = document.createElement("span");
 	close_button.classList.add("close-button");
 	li.append(close_button);
+
+	storage.add(li.textContent);
 
 	list.append(li);
 
@@ -24,7 +53,12 @@ list.addEventListener("click", event => {
 	let target = event.target;
 	if (!target.classList.contains("close-button")) return;
 
-	target.parentElement.remove();
+	let li = target.parentElement;
+	let index = [...li.parentElement.children].indexOf(li);
+
+	storage.remove(index);
+
+	li.remove();
 	updateCounter();
 });
 
@@ -32,8 +66,22 @@ function updateCounter() {
 	footer_text.textContent = `items total: ${list.children.length}`;
 }
 
-function init() {
-	updateCounter();
+function repopulate() {
+	let entries = storage.entries;
+	entries.forEach(entry => {
+		let li = document.createElement("li");
+		li.textContent = entry;
+		input_field.value = "";
+
+		let close_button = document.createElement("span");
+		close_button.classList.add("close-button");
+		li.append(close_button);
+
+		list.append(li);
+	});
 }
 
-init();
+(function init() {
+	repopulate();
+	updateCounter();
+})();
